b76e7e1bc21e273f80994adbd1fc67677b80c0d8
Jeremy <jeremyxu123@gmail.com>
2018-04-11 13:34:49 -0700

    framework

---
+++ server/models/dog.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dogSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 1,
+    unique: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+})
+
+const Dog = mongoose.model('Dog', dogSchema);
+
+module.exports = {Dog};
--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const {Dog} = require('./models/dog.js')
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+mongoose.Promise = global.Promise;
+const database = process.env.MONGODB_URI || "mongodb://localhost:27017/Doglist"
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send("GET /");
 })
 
+app.post('/dogs', (req, res) => {
+  res.send();
+})
 
 app.listen(port, () => {
   console.log( `Listening on port ${port}`);
