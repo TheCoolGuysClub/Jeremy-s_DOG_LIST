b25c833d0187a4e82647cee0a6e7985f1fa5650c
Jeremy <jeremyxu123@gmail.com>
2018-04-13 13:49:14 -0700

    done with friday's class

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const hbs = require('hbs');
+const path = require('path');
+const methodOverride = require('method-override');
+// const _ = require('lodash');
 const {Dog} = require('./models/dog.js')
 
+
 const app = express();
+
+//hbs setup
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../app/views/partials'));
+hbs.registerPartials(path.join(__dirname, '../app/views/partials'))
 
 //setup
 const port = process.env.PORT || 3000;
@@ -17,23 +27,48 @@ mongoose.connect(database)
   })
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-  res.send("GET /");
+  res.redirect('/dogs');
 })
 
+app.get('/dogs', (req, res) => {
+  Dog.find()
+    .then(dogs => {
+      res.send(dogs);
+    })
+    .catch(e => {
+      res.status(404).send();
+    })
+})
+
+app.get('/dogs/new', (req, res) => {
+    res.render('./dogs/new');
+})
+
+
+
 app.post('/dogs', (req, res) => {
+  if(!req.body.name|| !req.body.age) {
+    console.log('bad request');
+    res.status(400).send();
+  }
   const dog = new Dog({
-    name: 'Spot',
-    age: 2
+    name: req.body.name,
+    age: req.body.age
   })
   dog.save()
     .then(dog => {
-      res.send(dog);
+      res.redirect('/dogs');
     })
     .catch(e => {
       res.status(400).send();
     })
+})
+
+app.delete('/dogs/:id', (req, res) =. {
+    console.log('hit delete route');
 })
 
 app.listen(port, () => {
